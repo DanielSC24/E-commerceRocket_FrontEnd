1c30cf48d2a22a7da6a5f07e719a97013ad239b5
Carlos Martinez <carlosmtzstos@gmail.com>
2025-08-20 13:14:24 -0600

    cambios de modulos

--- E-commerceRocket/src/app/app-routing.module.ts
+++ E-commerceRocket/src/app/app-routing.module.ts
@@ -7,17 +7,19 @@ import { ProductosComponent } from './componentes/productos/productos.component'
 import { PedidosComponent } from './componentes/pedidos/pedidos.component';
 import { ClienteComponent } from './componentes/cliente/cliente.component';
 import { UsuariosComponent } from './componentes/usuarios/usuarios.component';
+import { ROLES } from './constants/constants';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
-  {path: 'dashboard', component: DashboardComponent,
+  {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard],
     children:[
-      {path: 'productos', component: ProductosComponent},
-      {path: 'pedidos', component: PedidosComponent},
-      {path: 'clientes', component: ClienteComponent},
-      {path: 'usuarios', component: UsuariosComponent}
+      {path: 'productos', component: ProductosComponent, canActivate: [AuthGuard]},
+      {path: 'pedidos', component: PedidosComponent, canActivate: [AuthGuard]},
+      {path: 'clientes', component: ClienteComponent, canActivate: [AuthGuard]},
+      {path: 'usuarios', component: UsuariosComponent, canActivate: [AuthGuard], data: { roles: [ROLES.ADMIN]}}
     ]
-  }
+  },
+  {path: '**', redirectTo: 'login'}
 ];
 
 @NgModule({
--- E-commerceRocket/src/app/guards/auth.guard.ts
+++ E-commerceRocket/src/app/guards/auth.guard.ts
@@ -1,18 +1,26 @@
 import { Injectable } from "@angular/core";
-import { CanActivate, Router} from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from "@angular/router";
 import { AuthService } from "../service/auth.service";
+import Swal from "sweetalert2";
 
 
 @Injectable({ providedIn: 'root'})
 export class AuthGuard implements CanActivate{
   constructor(private authService: AuthService, private router: Router){}
 
-  canActivate(): boolean {
+  canActivate(route: ActivatedRouteSnapshot, state:RouterStateSnapshot): boolean {
     if(this.authService.isLoggedIn()){
-      return true;
-    }
+    
     this.router.navigate(['/login']);
     return false;
 
   }
-}
+  const expectedRoles: string[] = route.data['roles'];
+  if(expectedRoles && !this.authService.hasAnyRole(expectedRoles)){
+    Swal.fire('Acceso denegado',
+      `Hola ${this.authService.getUsername()} no tienes permisos para acceder a esta sección`, 'warning');
+
+    return false;
+  }
+  return true;
+}}
